e5f1f72a4f349bd077878d4b1b3459d579a27fd4
William Skamser <skamser@iastate.edu>
2022-12-02 18:22:07 -0600

    EarthToMarsOpt

--- EarthToMars_LowThrust/ObjFunc.m
+++ EarthToMars_LowThrust/ObjFunc.m
@@ -1,74 +1,29 @@
 function MassUsed=ObjFunc(Thrust)
+%Read Run Time File
+RunTime=Thrust(end);
+MassFlowRate = 4.2231e-6; %4.2231e-5 must be a typo becuse it doesn't work???
+MassUsed=RunTime*MassFlowRate;
+
+
 %{
-Headerlines=6;
-%READ Thrust File
-file='../GeostationaryOrbit_LowThrust/ThrustProfileInitialGuess.thrust';
-fID=fopen(file,'r');
-A=textscan(fID, '%f %f %f %f %f', 'headerlines',Headerlines);
-ThrustProfile=cell2mat(A);
-fclose(fID);
-%}
-ISP=1500;
-g=9.80665;
-%mdot= Tmag/(ISP*g0)?
-MassUsed=0;
-
+    %Old Method
 %Converts back to Matrix;
-NumberOfSteps=100; 
-
+NumberOfSteps=200;
+TimeStep=(4.170266985e+05);
 %Thrust=zeros(((NumberOfSteps+1)*3),1);
 ThrustProfileNew(:,2)=Thrust(1:(NumberOfSteps+1));
 ThrustProfileNew(:,3)=Thrust( ((NumberOfSteps+1)+1) : ((NumberOfSteps+1)*2) );
 ThrustProfileNew(:,4)=Thrust( (((NumberOfSteps+1)*2)+1) : ((NumberOfSteps+1)*3) );
 %Converts Time step into time column
+
 for i=1:NumberOfSteps
-ThrustProfileNew(i+1,1)=ThrustProfileNew(i,1) + Thrust(i+ ((NumberOfSteps+1)*3));
+    ThrustProfileNew(i+1,1)=ThrustProfileNew(i,1) + Thrust(i+ ((NumberOfSteps+1)*3));
 end
 
-
-%ThrustProfileNew(:,1)=ThrustProfile(:,1);
-
-%ThrustProfileNew(:,2)=Thrust(1:11);
-%ThrustProfileNew(:,3)=Thrust(12:22);
-%ThrustProfileNew(:,4)=Thrust(23:33);
-%ThrustProfileNew(:,1)=ThrustProfile(:,1);
 for i=1:NumberOfSteps
-    ThrustProfileNew(i,5)=norm(ThrustProfileNew(i,2:4)) / (ISP * g); %mass flow rate 
+    ThrustProfileNew(i,5)=4.2231e-6; %4.2231e-5 must be a typo becuse it doesn't work???
     
     MassUsed=MassUsed+(ThrustProfileNew(i,5) * (ThrustProfileNew(i+1,1) - ThrustProfileNew(i,1)) );
 end
-
-%WRITE New Thrust File
-%{
-file2='../GeostationaryOrbit_LowThrust/ThrustProfile.thrust';
-S = fileread(file2);
-SS = regexp(S, '\r?\n', 'split');
-for i=1:NumberOfSteps
-    LineToChange = i+Headerlines; 
-    NewContent = compose("%.1f     \t%.10f %.10f %.10f  %.10f",ThrustProfileNew(i,1),ThrustProfileNew(i,2),ThrustProfileNew(i,3),ThrustProfileNew(i,4),ThrustProfileNew(i,5));
-    SS{LineToChange} = NewContent;
-end
-fid2 = fopen(file2, 'w');
-fprintf(fid2, '%s\n', SS{:});
-fclose(fid2);
-
-%RUN GMAT Script With New Thrust File
-Ans=gmat.gmat.RunScript();
-if Ans == 0
-    fprintf("Fail to run script");
-    FuelMass=NaN;
-    return
-end
-
-%READ Data File
-file1='../GeostationaryOrbit_LowThrust/Data.txt';
-fID1=fopen(file1,'r');
-B=textscan(fID1, '%f %f %f %f %f %f %f', 'headerlines',1);
-Data=cell2mat(B);
-fclose(fID1);
-
-TankMass_0=1000; %Mass of full tank
-FuelMass=TankMass_0-Data(6);
-%FuelMass=TankMass_0-MassUsed;
 %}
 end
--- EarthToMars_LowThrust/WriteInitialGuessFile.m
+++ EarthToMars_LowThrust/WriteInitialGuessFile.m
@@ -12,10 +12,9 @@ g=9.80665;
 NumberOfSteps=size(ThrustProfile(:,1));
 
 for i=1:NumberOfSteps(1)      
-    ThrustProfile(i,5)=norm(ThrustProfile(i,2:4)) / (ISP * g);
-    %ThrustProfile(i,5)=4.2231e-5;
+    %ThrustProfile(i,5)=norm(ThrustProfile(i,2:4)) / (ISP * g);
+    ThrustProfile(i,5)=4.2231e-6;
 end
-
 
 %WRITE New Thrust File
 Headerlines=6;
@@ -24,7 +23,7 @@ S = fileread(file2);
 SS = regexp(S, '\r?\n', 'split');
 for i=1:(NumberOfSteps(1))
     LineToChange = i+Headerlines; 
-    NewContent = compose("%8.4f\t\t\t\t%.9f %.9f %.9f  %.9f",ThrustProfile(i,1),ThrustProfile(i,2),ThrustProfile(i,3),ThrustProfile(i,4),ThrustProfile(i,5));
+    NewContent = compose("%8.4f\t\t\t\t%.10f %.10f %.10f  %.10f",ThrustProfile(i,1),ThrustProfile(i,2),ThrustProfile(i,3),ThrustProfile(i,4),ThrustProfile(i,5));
     %e       		w       2w       		e
     SS{LineToChange} = NewContent;
 end
