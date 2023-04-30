5a6c6a0b577478f38841b30c09fe0d131154c951
William Skamser <skamser@iastate.edu>
2023-04-30 18:54:06 -0500

    Save Work

--- GMAT_LowThrust_Function/LowThrustOutputStructToExcel.m
+++ GMAT_LowThrust_Function/LowThrustOutputStructToExcel.m
@@ -1,6 +1,12 @@
 function []=LowThrustOutputStructToExcel(O_Struct,FileName)
 NewExcelTable=insertAfter(pwd+FileName,strlength(pwd+FileName)-5,'_SolutionData');
 copyfile(pwd+FileName,NewExcelTable);
+%ClearRow
+xlswrite(NewExcelTable,strings(1000,1),1,'R2')
+xlswrite(NewExcelTable,strings(1000,3),1,'S2')
+xlswrite(NewExcelTable,strings(1000,1),1,'V2')
+xlswrite(NewExcelTable,strings(1000,1),1,'W2')
+%FillRow
 xlswrite(NewExcelTable,O_Struct.Time,1,'R2')
 xlswrite(NewExcelTable,O_Struct.ThrustXYZ,1,'S2')
 xlswrite(NewExcelTable,O_Struct.Alpha,1,'V2')
